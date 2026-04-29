a61b4949cc064dbbe5c243762faf9882300d281a
capTch <zhuh30@barker.college>
2026-04-30 05:27:05 +1000

    Small changes

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,28 @@
 </style>
 
 <app>
+  <navbar>
+    <div>
+      <h2>hz0</h2>
+    </div>
+  </navbar>
   <main>
-    <h1>Hi, I'm Harry</h1>
-    <h2>Welcome to my corner of the internet</h2>
-    <p>I write C and sometimes node</p>
+    <div class="namecard">
+      <div class="leftblock">
+        <h1>G'day, I'm Harry</h1>
+      </div>
+      <div class="rightblock">
+        <h2>Harry Zhu</h2>
+        <h3>/ˈhæɹi/</h3>
+        <h2>朱浩睿</h2>
+        <h3>/ˈtʃuː ˈhaʊ ˈrui/</h3>
+      </div>
+    </div>
+    <p>I write mostly C/C++ and sometimes Node, just here and there for web developent</p>
+
+    <div class="card">
+      <p>Feel free to explore my projects and get in touch!</p>
+    </div>
 
     <nav>
       <a href="https://github.com/harryzhu0">GitHub</a>
