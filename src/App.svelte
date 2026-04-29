965f98102316cde657e17704c28ec3d374b3684a
capTch <zhuh30@barker.college>
2026-04-30 04:55:18 +1000

    Basic website

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<style>
+  @import './app.css';
+</style>
+
+<app>
+  <main>
+    <h1>Hi, I'm Harry</h1>
+    <h2>Welcome to my corner of the internet</h2>
+    <p>I write C and sometimes node</p>
+
+    <nav>
+      <a href="https://github.com/harryzhu0">GitHub</a>
+      <a href="mailto:harryz@hz0.au">Email</a>
+    </nav>
+  </main>
+</app>
